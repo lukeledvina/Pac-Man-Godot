ef7653316d660a12e79ae27315419130428c1a47
Luke Ledvina <lukeledvina19@gmail.com>
2023-12-20 16:36:05 -0500

    Implemented pellet spawning programatically

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -3,6 +3,11 @@ extends Node2D
 @onready var player: CharacterBody2D = $Player
 @onready var pellet_container: Node2D = $Pellets
 
+var pellet_scene: PackedScene = preload("res://Scenes/pellet.tscn")
+var starting_pos: Vector2 = Vector2(12, 48)
+var current_pos: Vector2 = starting_pos
+var final_pos: Vector2 = Vector2(212, 272)
+
 func _on_left_zone_body_entered(_body):
 	player.global_position = Vector2(224, 152)
 
@@ -12,6 +17,25 @@ func _on_right_zone_body_entered(_body):
 	
 
 func _ready():
+	while current_pos.y <= final_pos.y:
+		var new_pellet = pellet_scene.instantiate()
+		pellet_container.add_child(new_pellet)
+		new_pellet.global_position = current_pos
+		
+		
+		
+		
+		
+		if current_pos.x >= final_pos.x:
+			current_pos.x = starting_pos.x
+			current_pos.y = current_pos.y + 8
+		else:
+			current_pos.x += 8
+		
+	
+	
+	
+	
 	var pellets = pellet_container.get_children()
 	
 	for pellet in pellets:
