461e2cf8d7a0350e9a0e9653d55dcae0570fab32
Luke Ledvina <lukeledvina19@gmail.com>
2023-12-21 17:22:48 -0500

    Added main menu

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/level.tscn")
+
+
+func _on_high_score_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_options_button_pressed():
+	pass # Replace with function body.
